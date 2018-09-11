c2c65f445e3837ec4064d36cd49147826995d454
huyinghuan <ec.huyinghuan@gmail.com>
2018-09-11 10:38:51 +0800

    add docker build

---
+++ docker-build.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+version=$1
+if [ "$1" == "" ]; then
+  version=latest
+fi
+
+docker build  -t docker.hunantv.com/huyinghuan/oauth:$version .
+
+if [ "$version" != "latest" ]; then
+    docker tag docker.hunantv.com/huyinghuan/oauth:$version docker.hunantv.com/huyinghuan/oauth:latest
+fi
